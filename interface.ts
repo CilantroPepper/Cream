7fdd38491611e186b20f2fbfbb7ab8280b247735
Cilantro Pepper <constnull@163.com>
2023-03-01 10:31:58 +0800

    [ADD] interface

---
+++ interface.ts
@@ -0,0 +1,36 @@
+import { Context, Next } from 'koa'
+export interface Result<T> {
+    items: T[],
+    fields: any
+}
+export interface PoolConfig {
+    host: string,
+    port: number,
+    database: string,
+    user: string,
+    password: string,
+    connectionLimit: number
+}
+export interface Base {
+    fetch<T extends object>(items: (keyof T)[], conditions: Record<string, any>[]): Promise<Result<T>>
+    put<T extends object>(entity: T): Promise<Result<object>>
+    remove(conditions: Record<string, any>): Promise<object>
+    update<T extends object>(entity: T, conditions: Record<string, any>[]): Promise<object>
+}
+export type Constructor<T> = new (...args: any[]) => T
+export type AppPlugins = ((ctx: Context, next: Next) => Promise<any> | any)[]
+export type ParamHandler = (p: { ctx: Context, type: string, key: string }) => any
+export type PropHandler = (p: { ctx: Context, type: string, key: string }) => any
+export interface CreamOptions {
+    controller: Constructor<any>[],
+    provider: Constructor<any>[],
+    plugins?: AppPlugins,
+}
+export interface CacheOptions {
+    stdTtl?: number,
+    checkTime?: number
+}
+export interface CreamConfig {
+    port: number;
+    database: PoolConfig;
+}
